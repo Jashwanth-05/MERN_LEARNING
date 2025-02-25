cc6548d7cb8dc2bc526de17d03a388b82931f53a
Jashwanth-05 <jashwanthj510@gmail.com>
2025-02-25 08:20:47 +0530

    Day 12 AN

--- SJIT_BACKEND/index.js
+++ SJIT_BACKEND/index.js
@@ -6,6 +6,10 @@ app.get("/",(req,res)=>(
     res.send("<h1>Welcome to Backend Server<h1/>")
 ))
 
+app.get("/static",(req,res)=>(
+    res.sendFile("C:/Users/ASUS/Documents/COLLEGE/MERN_LEARNING/BASIC HTML/index.html")
+))
+
 const PORT = 5555
 
 app.listen(PORT,()=>console.log("Server Started Successfully..."))
