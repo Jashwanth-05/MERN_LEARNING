83cec5ee3ff7891053a128e73dfc10d126b1ff8a
Jashwanth-05 <jashwanthj510@gmail.com>
2025-02-25 12:22:54 +0530

    Day 13 FN

--- SJIT_BACKEND/index.js
+++ SJIT_BACKEND/index.js
@@ -1,15 +1,47 @@
 const express= require('express')
 
+const mdb=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
+const Signup=require('./models/signupSchema')
 const app =express()
+app.use(express.json())
+const PORT = 5555
 
+mdb.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("MDB Connection Successful")
+}).catch((err)=>{
+    console.log("MDB Connection Failed", err)
+})
 app.get("/",(req,res)=>(
     res.send("<h1>Welcome to Backend Server<h1/>")
 ))
+
+app.post("/signup", async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        const newSignup = new Signup({
+            firstName,
+            lastName,
+            email,
+            password
+        });
+
+        await newSignup.save();
+
+        console.log("Signup Successful");
+        res.status(201).json({ message: "Signup Successful", isSignup: true });
+    } catch (err) {
+        console.error("Signup Error:", err);
+        res.status(500).json({ message: "Signup Unsuccessful", isSignup: false });
+    }
+});
 
 app.get("/static",(req,res)=>(
     res.sendFile("C:/Users/ASUS/Documents/COLLEGE/MERN_LEARNING/BASIC HTML/index.html")
 ))
 
-const PORT = 5555
+
 
 app.listen(PORT,()=>console.log("Server Started Successfully..."))
